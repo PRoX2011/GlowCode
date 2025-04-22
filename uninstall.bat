e1a353e3d4bb599782d763085bc6750cf352634d
PRoX <168980588+PRoX2011@users.noreply.github.com>
2025-04-22 13:25:16 +0500

    Update uninstall.bat

--- uninstall.bat
+++ uninstall.bat
@@ -4,7 +4,7 @@ chcp 1251 >nul
 REM Проверка доступности файла открытия
 openfiles >nul 2>&1
 if %errorlevel% NEQ 0 (
-    echo Пожалуйста, запустите этот скрипт от имени администратора!
+    echo Please run this script as administrator!
     pause
     exit /b 1
 )
@@ -13,15 +13,15 @@ set "INSTALL_DIR=%ProgramFiles%\GlowCode"
 set "DESKTOP=%USERPROFILE%\Desktop"
 set "SHORTCUT=%DESKTOP%\GlowCode.lnk"
 
-echo Удаление GlowCode из %INSTALL_DIR% ...
+echo Removing GlowCode from %INSTALL_DIR% ...
 del /F /Q "%INSTALL_DIR%\glowcode.exe"
 del /F /Q "%INSTALL_DIR%\glowcode.png"
 
-echo Удаление ярлыка с рабочего стола ...
+echo Removing a shortcut from the desktop ...
 del /F /Q "%SHORTCUT%"
 
 REM Удаление папки, если она пуста или существует
 rmdir "%INSTALL_DIR%" 2>nul
 
-echo Удаление завершено!
+echo Removal completed!
 pause
